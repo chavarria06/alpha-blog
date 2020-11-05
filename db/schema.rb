05f1fcd442574bb047735e1daea5b33e3f762bc0
Roberto Chavarria <robertomorais107@gmail.com>
2020-11-04 22:49:36 -0300

    Update wrongly generated DB migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_023236) do
+ActiveRecord::Schema.define(version: 2020_10_24_035932) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "date_columns", force: :cascade do |t|
   end
 
 end
