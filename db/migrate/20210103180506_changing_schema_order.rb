c9ae8ae16ca91d9afcc9915fdd1735b58f8e1bb8
Roberto Chavarria <robertomorais107@gmail.com>
2021-01-03 15:11:05 -0300

    migration testing

---
+++ db/migrate/20210103180506_changing_schema_order.rb
@@ -0,0 +1,4 @@
+class ChangingSchemaOrder < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_035728) do
+ActiveRecord::Schema.define(version: 2021_01_03_180506) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
+
 end
