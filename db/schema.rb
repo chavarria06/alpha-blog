e5b952d175a4586d4aea1a8f4f4041a627932b62
Roberto Chavarria <robertomorais107@gmail.com>
2021-01-03 14:49:36 -0300

    updating schema order

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,8 @@ ActiveRecord::Schema.define(version: 2020_12_29_035728) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "description"
   end
-
 end
