ff0a49884868579c4223eea1097c6af0e3290fd8
Roberto Chavarria <robertomorais107@gmail.com>
2021-02-07 01:15:01 -0300

    adding 'delete users & associated articles' feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    before_action :set_users, only: [:show, :edit, :update]
+    before_action :set_users, only: [:show, :edit, :update, :destroy]
     before_action :require_user, only: [:edit, :update]
-    before_action :require_same_user, only:  [:edit, :update]
+    before_action :require_same_user, only:  [:edit, :update, :destroy]
 
 
     def show 
@@ -39,6 +39,12 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        @user.destroy
+        session[:user_id] = nil
+        flash[:notice] = "Account and associated articles deleted"
+    end
+    
     private 
 
     def set_users
