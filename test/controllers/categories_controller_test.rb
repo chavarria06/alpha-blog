4950c02918f3b948e22a369974024d552af237cd
Roberto Chavarria <robertomorais107@gmail.com>
2021-02-28 15:34:49 -0300

    adding integration tests for categories and updating navigation partial

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @category = Category.create(name: "Sports")
+    @admin_user = User.create(username: "johndoe", email: "johndoe@example.com", password: "password", admin: true)
   end
 
   test "should get index" do
@@ -11,15 +12,16 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    sign_in_as(@admin_user)
     get new_category_url
     assert_response :success
   end
 
   test "should create category" do
+    sign_in_as(@admin_user)
     assert_difference('Category.count', 1) do
       post categories_url, params: { category: { name: "Travel"} }
     end
-
     assert_redirected_to category_url(Category.last)
   end
 
@@ -27,6 +29,14 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     get category_url(@category)
     assert_response :success
   end
+
+  test "should not create category if not admin" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: "Travel"} }
+    end
+    assert_redirected_to categories_url
+  end
+
 
   # test "should get edit" do
   #   get edit_category_url(@category)
--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
-  test "get new category and create category" do
+
+  setup do
+    @admin_user = User.create(username: "johndoe", email: "johndoe@example.com", password: "password", admin: true)
+    sign_in_as(@admin_user)
+  end
+
+  test "get new category form and create category" do
     get "/categories/new"
     assert_response :success
     assert_difference 'Category.count', 1 do
@@ -12,4 +18,15 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match "Sports", response.body
   end
+
+  test "get new category form and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: {name: " "} }
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end 
 end
